c66fbfc41945d7bfddbb8009f1f2103266c9ed20
lew21027 <lew21027@byui.edu>
2022-01-18 21:06:24 -0500

    initial commit

--- js/dates.js
+++ js/dates.js
@@ -2,4 +2,4 @@ const lastmod = document.querySelector('#lastmod');
 lastmod.innerHTML = `Last Updated: ${document.lastModified}`;
 
 const year = document.getElementById("year").innerHTML = new Date().getFullYear();
-year.innerHTML = `&copy;${document.lastModified};`
+year.innerHTML = `&copy${document.lastModified};`
